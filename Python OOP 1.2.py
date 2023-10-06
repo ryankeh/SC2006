186b55e6afecdd8e5270e3fbcf0f849764be47f0
Ryan Keh <113022074+ryankeh@users.noreply.github.com>
2023-10-06 17:41:48 +0800

    Update Python OOP 1.2.py

--- Python OOP 1.2.py
+++ Python OOP 1.2.py
@@ -9,15 +9,20 @@ class Employee:
         self.pay = pay
         self.email = first + '.' + last + '@company.com'
         
+        Employee.num_of_emps += 1
+        
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
         
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
         
+print(Employee.num_of_emps)
+
 emp_1 = Employee('Winfred','Cheok',50000)
 emp_2 = Employee('Melvin','Lim',60000)
 
+print(Employee.num_of_emps)
 print(emp_1.__dict__)
 
 print(emp_1.email)
@@ -34,7 +39,3 @@ emp_1.raise_amount = 1.06
 print(Employee.raise_amount)
 print(emp_1.raise_amount)
 print(emp_2.raise_amount)
-
-
-
-
