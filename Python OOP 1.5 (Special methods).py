48b9282386336f68b01d78833de9f22d9830c3df
Ryan Keh <113022074+ryankeh@users.noreply.github.com>
2023-10-07 23:21:52 +0800

    Update Python OOP 1.5 (Special methods).py

--- Python OOP 1.5 (Special methods).py
+++ Python OOP 1.5 (Special methods).py
@@ -17,17 +17,26 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
     
+    #the 2 methods below change how our objects are printed and displayed
     #for assisting developers to debug and work on code
     def __repr__(self):
         return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
-        
-        
     #for a readable piece of information
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        return '{} - {}'.format(self.fullname(), self.email)
+    
+    #used to define the "+" operand
+    def __add__(self, other):
+        return self.pay + other.pay
 
         
 emp_1 = Employee('Winfred','Cheok',50000)
 emp_2 = Employee('Melvin','Lim',60000)
 
-print(emp_1)
+# print(emp_1)
+print(emp_1.__repr__())
+print(emp_1.__str__())
+#demonstrating the "__add__" method
+print(emp_1 + emp_2)
+
+
